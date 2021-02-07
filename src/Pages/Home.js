9707d89114bb9c252fc3e9d61ce0ba0a0815e8fa
Abhishek <abhishek.sadhu@outlook.com>
2021-02-07 12:53:53 -0500

    mediaquery-thirdcommit
    Added media queries for min-width 1200px

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -34,12 +34,12 @@ const Home = () => {
         restInfo="Suspendisse potenti. Aliquam tincidunt placerat neque, eu eleifend elit porttitor sit amet. Proin varius orci diam, a finibus augue finibus rutrum. Ut interdum a nunc non dapibus. Nam sollicitudin cursus nibh in congue. Morbi cursus neque ligula, vitae ultricies mi venenatis eget. Sed sollicitudin libero neque, in faucibus lectus tristique quis. Suspendisse et iaculis neque, ac tempor justo. In cursus placerat accumsan. Etiam sed justo quis odio vulputate iaculis vel sed risus."
         Isource="/DescImages/Kitchen.jpg"
       />
+      <Patio />
       <RestDesc
         header="THE BAR"
         restInfo="Nullam neque metus, condimentum a urna ac, condimentum semper sem. Vivamus nibh mauris, scelerisque non nisi a, ultrices mollis nunc. Donec mollis at arcu eget placerat. Vivamus vel purus quis nulla venenatis tempus vel a elit. Curabitur sollicitudin pharetra risus, nec venenatis quam condimentum a. "
         Isource="/DescImages/bar.jpg"
       />
-      <Patio />
       <HomeEnd />
     </div>
   );
